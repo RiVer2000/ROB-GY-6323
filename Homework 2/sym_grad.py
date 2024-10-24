5969ee307573e82c7e0bc2c7d429a0e10a9797a7
RiVer2000 <vermarishabh2000@gmail.com>
2024-10-24 14:47:14 -0400

    debugging ex3

--- Homework 2/sym_grad.py
+++ Homework 2/sym_grad.py
@@ -1,33 +1,22 @@
 import sympy as sp
 
-def symbolic_gradient(f, variables):
-    """
-    Compute the symbolic gradient (partial derivatives) of a function.
-    """
-    gradient = [sp.diff(f, var) for var in variables]
+def gradient(f, variables):
+    gradient = sp.Matrix([sp.simplify(sp.diff(f, var)) for var in variables])
     return gradient
 
-def symbolic_hessian(f, variables):
-    """
-    Compute the symbolic Hessian matrix (second-order partial derivatives) of a function.
-    """
-    hessian = sp.Matrix([[sp.diff(f, var1, var2) for var2 in variables] for var1 in variables])
+def hessian(f, variables):
+    hessian = sp.Matrix([[sp.simplify(sp.diff(f, var1, var2)) for var2 in variables] for var1 in variables])
     return hessian
-
-# Define symbolic variables
 x, y, z = sp.symbols('x y z')
-
-# Define the functions from the image
 f1 = -sp.exp(-(x - 1)**2)
 f2 = (1 - x)**2 + 100 * (y - x**2)**2
-f3 = sp.Matrix([x, y]).T * sp.Matrix([[3, 1], [1, 3]]) * sp.Matrix([x, y]) + sp.Matrix([1, -1]).T * sp.Matrix([x, y])
-f4 = 1/2 * sp.Matrix([x, y, z]).T * sp.Matrix([[1, 1, 0], [1, 1, 0], [0, 0, 4]]) * sp.Matrix([x, y, z]) - sp.Matrix([0, 0, 1]).T * sp.Matrix([x, y, z])
+f3 = (sp.Matrix([x, y]).T * sp.Matrix([[3, 1], [1, 3]]) * sp.Matrix([x, y])).doit() + (sp.Matrix([1, -1]).T * sp.Matrix([x, y])).doit()
 
-# Calculate gradients
-grad_f1 = symbolic_gradient(f1, [x])
-grad_f2 = symbolic_gradient(f2, [x, y])
-grad_f3 = symbolic_gradient(f3, [x, y])
-grad_f4 = symbolic_gradient(f4, [x, y, z])
+f4 = (1/2 * sp.Matrix([x, y, z]).T * sp.Matrix([[1, 1, 0], [1, 1, 0], [0, 0, 4]]) * sp.Matrix([x, y, z])).doit() - (sp.Matrix([0, 0, 1]).T * sp.Matrix([x, y, z])).doit()
+grad_f1 = gradient(f1, [x])
+grad_f2 = gradient(f2, [x, y])
+grad_f3 = gradient(f3, [x, y])
+grad_f4 = gradient(f4, [x, y, z])
 
 print("Gradient of f1:")
 sp.pprint(grad_f1)
@@ -38,12 +27,11 @@ sp.pprint(grad_f3)
 print("Gradient of f4:")
 sp.pprint(grad_f4)
 
-
 # Calculate Hessians
-hess_f1 = symbolic_hessian(f1, [x])
-hess_f2 = symbolic_hessian(f2, [x, y])
-hess_f3 = symbolic_hessian(f3, [x, y])
-hess_f4 = symbolic_hessian(f4, [x, y, z])
+hess_f1 = hessian(f1, [x])
+hess_f2 = hessian(f2, [x, y])
+hess_f3 = hessian(f3, [x, y])
+hess_f4 = hessian(f4, [x, y, z])
 
 print("Hessian of f1:")
 sp.pprint(hess_f1)
